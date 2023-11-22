94f2a7958726afa50114e0083da12403e6121a4a
jamiechien <jamiechien@fstop.com.tw>
2023-11-22 17:15:34 +0800

    修改 : 台灣地圖 : 畫面調整

--- map-lib/src/lib/map-lib/map-lib.component.ts
+++ map-lib/src/lib/map-lib/map-lib.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import * as d3 from 'd3';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import * as topojson from 'topojson-client';
-import {BaseType} from "d3";
+import { BaseType } from "d3";
 
 @Component({
   selector: 'org-map-lib',
@@ -16,12 +16,33 @@ export class MapLibComponent implements OnInit {
 
   @Input() data?: any;
 
+  areaInfo?: any[] = [{
+    area:'台北市',
+    p1:'鄭紫雲',
+    p2:'郭曉風',
+    p3:'郭文山',
+    p1rate:'57%',
+    p2rate:'38.1%',
+    p3rate:'61.2%',
+    pastElection2020:'53.7%',
+    pastElection2018:'49.5%',
+    pastElection2016:'61.2%',
+  }];
+
+
+
   ngOnInit(): void {
-    let svg = d3.select("svg")
-    console.log(svg)
-    let g = svg.append("g")
-    let projectMethod = d3.geoMercator().center([123, 24]).scale(5500);
+    let svg = d3.select("svg");
+    console.log(svg);
+    let g = svg.append("g");
+    let projectMethod = d3.geoMercator().center([123, 24]).scale(8000);
     let pathGenerator: any = d3.geoPath().projection(projectMethod);
+
+    let customColors = ["#EEC020", "#5195C7", "#6CC25E", "#9DD195", "#ABC6DA","#EAD897","#84D677"];
+    // // 創建一個顏色比例尺
+    // let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    let colorScale = d3.scaleOrdinal().range(customColors);
+
     d3.json('../../assets/taiwan.json')
       .then((data: any) => {
         console.log("geometries", data.objects["COUNTY_MOI_1090820"].geometries)
@@ -37,9 +58,11 @@ export class MapLibComponent implements OnInit {
           .append("path")
           .attr("d", pathGenerator)
           .attr("class", "county")
+          // 使用顏色比例尺為每個區塊選擇顏色
+          .attr("fill", (d: any, i: any) => String(colorScale(i)))
           // 加上簡易版本 tooltip
           .append("title")
           .text((d: any) => d.properties["COUNTYNAME"])
-      })
+      });
   }
 }

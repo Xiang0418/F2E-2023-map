886fd1928e85f30535a353d252e67d1ffe89d340
Kris <87007257+kris04090513@users.noreply.github.com>
2023-11-26 16:20:12 +0800

    新增 menu 切換

--- apps/f2e-map/src/app/app.component.ts
+++ apps/f2e-map/src/app/app.component.ts
@@ -1,22 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import {CountyDetailComponent, MapLibComponent} from "@org/map-lib";
-import * as data from '../assets/taiwan.json'
-import {JsonPipe} from "@angular/common";
+import { CountyDetailComponent, MapLibComponent } from '@org/map-lib';
+import * as data from '../assets/taiwan.json';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MapLibComponent, JsonPipe,CountyDetailComponent],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    MapLibComponent,
+    JsonPipe,
+    CountyDetailComponent,
+    NgIf,
+  ],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'f2e-map';
-  a = data
+  a = data;
+  show: boolean = false;
   ngOnInit(): void {
-    console.log(data)
+    console.log(data);
   }
-
 }

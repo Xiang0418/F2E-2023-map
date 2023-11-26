7c68e747907151fcc26abbf03441ae858aa1a472
jamiechien <jamiechien@fstop.com.tw>
2023-11-26 15:43:46 +0800

    修改 : 台灣地圖調整加資料

--- map-lib/src/lib/map-lib/map-lib.component.ts
+++ map-lib/src/lib/map-lib/map-lib.component.ts
@@ -17,18 +17,122 @@ export class MapLibComponent implements OnInit {
   @Input() data?: any;
 
   areaInfo?: any[] = [{
-    area:'台北市',
-    p1:'鄭紫雲',
-    p2:'郭曉風',
-    p3:'郭文山',
-    p1rate:'57%',
-    p2rate:'38.1%',
-    p3rate:'61.2%',
-    pastElection2020:'53.7%',
-    pastElection2018:'49.5%',
-    pastElection2016:'61.2%',
+    area: '臺北市',
+    p1: '鄭紫雲',
+    p2: '郭曉風',
+    p3: '郭文山',
+    p1rate: '57%',
+    p2rate: '38.1%',
+    p3rate: '61.2%',
+    pastElection2020: '53.7%',
+    pastElection2018: '49.5%',
+    pastElection2016: '61.2%',
+  },
+  {
+    area: '花蓮縣',
+    p1: '鄭紫雲',
+    p2: '郭曉風',
+    p3: '郭文山',
+    p1rate: '27%',
+    p2rate: '18.1%',
+    p3rate: '81.2%',
+    pastElection2020: '23.7%',
+    pastElection2018: '39.5%',
+    pastElection2016: '41.2%',
   }];
 
+  colorArray: any[] = [
+    {
+      area: '臺北市',
+      fill: '#F0D574',
+    },
+    {
+      area: '新北市',
+      fill: '#5195C7',
+    },
+    {
+      area: '桃園市',
+      fill: '#EBD070',
+    },
+    {
+      area: '臺中市',
+      fill: '#84D677',
+    },
+    {
+      area: '臺南市',
+      fill: '#EAD897',
+    },
+    {
+      area: '高雄市',
+      fill: '#6CC25E',
+    },
+    {
+      area: '新竹縣',
+      fill: '#6CC25E',
+    },
+    {
+      area: '苗栗縣',
+      fill: '#85C979',
+    },
+    {
+      area: '彰化縣',
+      fill: '#ECC848',
+    },
+    {
+      area: '南投縣',
+      fill: '#EEC020',
+    },
+    {
+      area: '雲林縣',
+      fill: '#ABC6DA',
+    },
+    {
+      area: '嘉義縣',
+      fill: '#9DD195',
+    },
+    {
+      area: '屏東縣',
+      fill: '#EEC020',
+    },
+    {
+      area: '宜蘭縣',
+      fill: '#EEC020',
+    },
+    {
+      area: '花蓮縣',
+      fill: '#6FA5CD',
+    },
+    {
+      area: '臺東縣',
+      fill: '#8DB6D4',
+    },
+    {
+      area: '澎湖縣',
+      fill: '#F0D574',
+    },
+    {
+      area: '金門縣',
+      fill: '#F0D574',
+    },
+    {
+      area: '連江縣',
+      fill: '#F0D574',
+    },
+    {
+      area: '基隆市',
+      fill: '#6CC25E',
+    },
+    {
+      area: '新竹市',
+      fill: '#E8DFBF',
+    },
+    {
+      area: '嘉義市',
+      fill: '#E8DFBF',
+    },
+  ]
+
+  newInfo?: any[] = []
 
 
   ngOnInit(): void {
@@ -37,11 +141,6 @@ export class MapLibComponent implements OnInit {
     let g = svg.append("g");
     let projectMethod = d3.geoMercator().center([123, 24]).scale(8000);
     let pathGenerator: any = d3.geoPath().projection(projectMethod);
-
-    let customColors = ["#EEC020", "#5195C7", "#6CC25E", "#9DD195", "#ABC6DA","#EAD897","#84D677"];
-    // // 創建一個顏色比例尺
-    // let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-    let colorScale = d3.scaleOrdinal().range(customColors);
 
     d3.json('../../assets/taiwan.json')
       .then((data: any) => {
@@ -54,15 +153,21 @@ export class MapLibComponent implements OnInit {
 
         g.append("path")
         const paths = g.selectAll("path").data(geometries.features);
+
         paths.enter()
           .append("path")
           .attr("d", pathGenerator)
           .attr("class", "county")
-          // 使用顏色比例尺為每個區塊選擇顏色
-          .attr("fill", (d: any, i: any) => String(colorScale(i)))
-          // 加上簡易版本 tooltip
+          .attr("fill", (d: any) => this.colorArray.find((x)=>x.area === d.properties.COUNTYNAME).fill)
+          .on('click', (event, d: any) => {
+            const externalData = this.areaInfo;
+            this.newInfo = externalData?.filter(x => x.area === d.properties.COUNTYNAME)
+          })
           .append("title")
           .text((d: any) => d.properties["COUNTYNAME"])
       });
+
+
+
   }
 }
